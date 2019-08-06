e04179c90174ed83bcaff92069484f3de6039f1d
Devon Mack <devon.mack@shopify.com>
2019-08-06 12:23:05 -0400

    unique

--- app/models/tracked_item.rb
+++ app/models/tracked_item.rb
@@ -1,4 +1,17 @@
 class TrackedItem < ApplicationRecord
     belongs_to :user
     has_many :price_queries
+
+    def check_threshold(price)
+        if price < threshold
+            logger.info("EMAIL TO " + user.email)
+        end
+    end
+
+    def unique?
+        unless user
+            user = current_user
+        end
+        current_user.tracked_items.where(url: @tracked_item.url).count < 1
+    end
 end
