b003ac73f1ad663e41abdb80b49812ae66846ac3
Devon Mack <devon.mack@shopify.com>
2019-09-10 22:03:07 -0400

    Rescue timeout

--- app/models/tracked_item.rb
+++ app/models/tracked_item.rb
@@ -58,7 +58,12 @@ class TrackedItem < ApplicationRecord
       until resp or attempts >= 2
         logger.info("Attempting to query #{url}")
         attempts += 1
-        resp = open(url, open_timeout: 15, proxy: ProxyUrl.get_proxy, 'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
+        begin
+          resp = open(url, open_timeout: 15, proxy: ProxyUrl.get_proxy, 'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
+        end
+        rescue Timeout::Error
+          logger.debug "Timed out."
+        end
       end
 
       logger.info("Success") if resp
